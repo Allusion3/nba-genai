9e8e6d605828c3a560f3c720eef44cd3e9a82d79
Jean DeZonia <jsdezonia3@gmail.com>
2025-04-11 19:56:59 -0500

    Initial commit - fresh Spring Boot setup

---
+++ src/main/java/com/jeandezonia/nba/NbaGenaiApplication.java
@@ -0,0 +1,13 @@
+package com.jeandezonia.nba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NbaGenaiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NbaGenaiApplication.class, args);
+	}
+
+}
